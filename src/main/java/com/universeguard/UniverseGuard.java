3962e2a56aa3e614071078fe213af326a3334428
Jimi <Jimi@DESKTOP-P5UJG5T.station>
2018-01-14 15:13:29 +0100

    Updated version

--- src/main/java/com/universeguard/UniverseGuard.java
+++ src/main/java/com/universeguard/UniverseGuard.java
@@ -57,7 +57,7 @@ public class UniverseGuard {
 	/**
 	 * Plugin Version
 	 */
-	public static final String VERSION = "2.4.1";
+	public static final String VERSION = "2.5";
 	/**
 	 * Plugin ID
 	 */
