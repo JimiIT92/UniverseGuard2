6248db441f8c69c6a6d865effe00963b18d12b5e
jimiit92 <jimiit92@gmail.com>
2019-08-08 20:39:43 +0200

    Fix issue #75

--- src/main/java/com/universeguard/event/flags/FlagDestroyListener.java
+++ src/main/java/com/universeguard/event/flags/FlagDestroyListener.java
@@ -55,10 +55,7 @@ public class FlagDestroyListener {
 		Entity targetEntity = event.getTargetEntity();
 		EntityType type = targetEntity.getType();
 		if(FlagUtils.isBlockEntity(type) || targetEntity instanceof TileEntity) {
-			if(!this.handleEvent(event, targetEntity.getLocation(), player)) {
-				//LogUtils.log("SPAWN");
-			    //player.getWorld().spawnEntity(targetEntity);
-            }
+			this.handleEvent(event, targetEntity.getLocation(), player);
 		}
 	}
 
