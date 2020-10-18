cf44d8a369e8cc84a0faca2de8ee8fadde28a501
jimiit92 <jimiit92@gmail.com>
2020-10-18 19:09:22 +0200

    Partial fix for pistons. Pistons will remain powered if they touch a block they cannot pull or push

--- src/main/java/com/universeguard/event/flags/FlagPistonsListener.java
+++ src/main/java/com/universeguard/event/flags/FlagPistonsListener.java
@@ -3,13 +3,13 @@ package com.universeguard.event.flags;
 import com.universeguard.region.enums.EnumRegionFlag;
 import com.universeguard.region.enums.RegionEventType;
 import com.universeguard.utils.RegionUtils;
+import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.block.BlockTypes;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Cancellable;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.block.ChangeBlockEvent;
-import org.spongepowered.api.event.filter.cause.First;
 import org.spongepowered.api.event.filter.cause.Root;
 import org.spongepowered.api.world.LocatableBlock;
 import org.spongepowered.api.world.Location;
@@ -18,14 +18,19 @@ import org.spongepowered.api.world.World;
 public class FlagPistonsListener {
 
     @Listener
-    public void onPiston(ChangeBlockEvent.Pre event, @Root LocatableBlock block, @First Player player) {
-        if(block.getBlockState().getType().equals(BlockTypes.PISTON) ||
-                block.getBlockState().getType().equals(BlockTypes.PISTON_EXTENSION)||
-                block.getBlockState().getType().equals(BlockTypes.PISTON_HEAD) ||
-                block.getBlockState().getType().equals(BlockTypes.STICKY_PISTON) ||
+    public void onPistons(ChangeBlockEvent.Pre event, @Root LocatableBlock block) {
+        if(isPiston(block.getBlockState().getType()) ||
                 block.get(Keys.EXTENDED).isPresent()) {
-            this.handleEvent(event, block.getLocation(), player);
+            Player player = event.getCause().first(Player.class).orElse(null);
+            event.setCancelled(event.getLocations().stream().anyMatch(location -> this.handleEvent(event, location, player)));
         }
+    }
+
+    private boolean isPiston(BlockType type) {
+        return type.equals(BlockTypes.PISTON) ||
+                type.equals(BlockTypes.PISTON_EXTENSION)||
+                type.equals(BlockTypes.PISTON_HEAD) ||
+                type.equals(BlockTypes.STICKY_PISTON);
     }
 
     private boolean handleEvent(Cancellable event, Location<World> location, Player player) {
